c3f3df2064f585675b6ac11a031e32e5e14aac79
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-06 19:02:38 +0700

    xong

--- db.js
+++ db.js
@@ -2,10 +2,8 @@ const mongoose = require('mongoose');
 
 const uri = 'mongodb://localhost/node0409';
 
-mongoose.connect(uri, { 
-    useMongoClient: true, 
-    promiseLibrary: global.Promise 
-});
+mongoose.connect(uri, { useMongoClient: true });
+mongoose.Promise = global.Promise;
 
 const ProductSchema = new mongoose.Schema({
     name: { type: String },
@@ -16,7 +14,26 @@ const ProductSchema = new mongoose.Schema({
 
 const Product = mongoose.model('product', ProductSchema);
 
-Product.find({})
+// Product.find({})
+// .then(products => console.log(products));
+
+// Product.findById('59d764a3e9c1ff47f89c0dd0')
+// .then(product => console.log(product));
+
+// Product.find({ image: '3.jpg' })
+// .then(product => console.log(product));
+
+// const pr = new Product({ name: 'EDFG' });
+// pr.save()
+// .then((x) => console.log(x));
+
+// Product.findByIdAndRemove('59d76f9e1d53efe983c776aa')
+// .then(x => console.log(x));
+
+// Product.findByIdAndUpdate('59d76f974025fee9782a2ee9', { name: 'edfg' })
+// .then(x => console.log(x));
+
+Product.findById('59d76f974025fee9782a2ee9', { name: 'edfg' })
 .then(x => console.log(x));
 
 // mongoose.connection.once('open', () => console.log('db started'))
